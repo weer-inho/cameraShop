63eac03d2e6d55fcb5c7d760c39b7eb87138b391
Руслан <60976203+weer-inho@users.noreply.github.com>
2022-11-29 20:31:05 +0300

    тесты для компонента app

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -1,8 +1,57 @@
 import {render, screen} from '@testing-library/react';
+import {createMemoryHistory} from 'history';
+import {Provider} from 'react-redux';
+import {configureMockStore} from '@jedmao/redux-mock-store';
 import App from './app';
+import {NameSpace} from "../../utils/const";
+import {makeFakeCameraOffer, makeFakeReview} from "../../utils/mocks";
 
-test('Renders app-component', () => {
-  render(<App />);
-  const textElement = screen.getByText(/Hello, world!/i);
-  expect(textElement).toBeInTheDocument();
+const mockStore = configureMockStore();
+
+const store = mockStore({
+  [NameSpace.AllData]: {
+    cameras: [makeFakeCameraOffer()],
+    pageCount: 1,
+    promo: makeFakeCameraOffer(),
+    isDataLoaded: false,
+  },
+  [NameSpace.OfferData]: {
+    currentOffer: {},
+    currentComments: [makeFakeReview(), makeFakeReview()],
+    currentNearBy: makeFakeCameraOffer()
+  }
+});
+
+const history = createMemoryHistory();
+
+const fakeApp = (
+  <Provider store={store}>
+      <App />
+  </Provider>
+);
+
+describe('Application Routing', () => {
+  it('should render "Main" when user navigate to "/"', () => {
+    history.push('/');
+
+    render(fakeApp);
+
+    expect(screen.getByText(/Каталог фото- и видеотехники/i)).toBeInTheDocument();
+  });
+
+  it('should render "Main" when user navigate to "/catalog/page_1"', () => {
+    history.push('/catalog/page_1');
+
+    render(fakeApp);
+
+    expect(screen.getByText(/Каталог фото- и видеотехники/i)).toBeInTheDocument();
+  });
+
+  it('should render "Camera" when user navigate to "cameras/:id"', () => {
+    history.push('cameras/1');
+
+    render(fakeApp);
+
+    expect(screen.getByText(/₽/i)).toBeInTheDocument();
+  });
 });
