7bec1259f5f6197c9a04a400fe70f00bb02c71c8
Руслан <60976203+weer-inho@users.noreply.github.com>
2022-12-01 10:29:22 +0300

    добавлен тест для cameraListWrapper

---
+++ src/components/cameras-list-wrapper/camera-list-wrapper.test.tsx
@@ -0,0 +1,34 @@
+import {render, screen} from '@testing-library/react';
+import CamerasListWrapper from './cameras-list-wrapper';
+import {Provider} from 'react-redux';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {makeFakeCameraOffer, makeFakePromoOffer} from '../../utils/mocks';
+import {NameSpace} from "../../utils/const";
+import {BrowserRouter as Router} from 'react-router-dom';
+
+const mockStore = configureMockStore();
+
+const store = mockStore({
+  [NameSpace.AllData]: {
+    cameras: [makeFakeCameraOffer(), makeFakeCameraOffer()],
+    pageCount: 1,
+    promo: makeFakePromoOffer(),
+    isDataLoaded: false,
+  }
+});
+
+const fakeApp = (
+  <Provider store={store}>
+    <Router>
+      <CamerasListWrapper />
+    </Router>
+  </Provider>
+);
+
+describe('Component: CamerasListWrapper', () => {
+  it('should render correctly', () => {
+    render(fakeApp);
+
+    expect(screen.getByText(/Сортировать:/i)).toBeInTheDocument();
+  });
+});
