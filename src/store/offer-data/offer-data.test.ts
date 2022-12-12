50cdc1e3a545c16e361458638061ab33f5bb1322
Руслан <60976203+weer-inho@users.noreply.github.com>
2022-12-12 19:55:09 +0300

    поправил тесты длы offer-data

--- src/store/offer-data/offer-data.test.ts
+++ src/store/offer-data/offer-data.test.ts
@@ -3,36 +3,36 @@ import {cameraType, reviewType} from '../../types/types';
 import {makeFakeCameraOffer, makeFakeReview} from '../../utils/mocks';
 import {createCommentAction, fetchOfferAction, fetchOfferCommentsAction, fetchOffersNearByAction} from '../api-actions';
 
-const fakeOffer = [makeFakeCameraOffer()];
-const fakeReview = [makeFakeReview()];
+const fakeOffer = makeFakeCameraOffer();
+const fakeReviews = [makeFakeReview()];
 
 describe('Reducer: offerData', () => {
   it('without additional parameters should return initial state', () => {
     expect(offerData.reducer(void 0, {type: 'UNKNOWN_ACTION'}))
-      .toEqual({currentOffer: {}, currentComments: {},currentNearBy: {}});
+      .toEqual({currentOffer: {}, currentComments: [],currentNearBy: {}});
   });
 
   it('should update currentOffer by load offer', () => {
-    const state = {currentOffer: {} as cameraType, currentComments: {} as reviewType[], currentNearBy: {} as cameraType[]};
+    const state = {currentOffer: {} as cameraType, currentComments: [] as reviewType[], currentNearBy: {} as cameraType[]};
     expect(offerData.reducer(state, {type: fetchOfferAction.fulfilled.type, payload: fakeOffer}))
-      .toEqual({currentOffer: fakeOffer, currentComments: {}, currentNearBy: {}});
+      .toEqual({currentOffer: fakeOffer, currentComments: [], currentNearBy: {}});
   });
 
   it('should update currentComments by load comments', () => {
-    const state = {currentOffer: {} as cameraType, currentComments: {} as reviewType[], currentNearBy: {} as cameraType[]};
-    expect(offerData.reducer(state, {type: fetchOfferCommentsAction.fulfilled.type, payload: fakeReview}))
-      .toEqual({currentOffer: {}, currentComments: fakeReview, currentNearBy: {}});
+    const state = {currentOffer: {} as cameraType, currentComments: [] as reviewType[], currentNearBy: {} as cameraType[]};
+    expect(offerData.reducer(state, {type: fetchOfferCommentsAction.fulfilled.type, payload: fakeReviews}))
+      .toEqual({currentOffer: {}, currentComments: fakeReviews, currentNearBy: {}});
   });
 
   it('should update currentNearBy by load offersNearBy', () => {
-    const state = {currentOffer: {} as cameraType, currentComments: {} as reviewType[], currentNearBy: {} as cameraType[]};
+    const state = {currentOffer: {} as cameraType, currentComments: [] as reviewType[], currentNearBy: {} as cameraType[]};
     expect(offerData.reducer(state, {type: fetchOffersNearByAction.fulfilled.type, payload: fakeOffer}))
-      .toEqual({currentOffer: {} as cameraType, currentComments: {} as reviewType[], currentNearBy: fakeOffer});
+      .toEqual({currentOffer: {} as cameraType, currentComments: [], currentNearBy: fakeOffer});
   });
 
   it('should update currentComments by create comment', () => {
     const state = {currentOffer: {} as cameraType, currentComments: [] as reviewType[], currentNearBy: {} as cameraType[]};
-    expect(offerData.reducer(state, {type: createCommentAction.fulfilled.type, payload: fakeReview}))
-      .toEqual({currentOffer: {}, currentComments: [fakeReview, fakeReview], currentNearBy: {}});
+    expect(offerData.reducer(state, {type: createCommentAction.fulfilled.type, payload: fakeReviews}))
+      .toEqual({currentOffer: {}, currentComments: [fakeReviews], currentNearBy: {}});
   });
 });
