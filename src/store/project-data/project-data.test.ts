52ab8ec69d9440b3235c83030ca1415e945defba
Руслан <60976203+weer-inho@users.noreply.github.com>
2022-12-12 19:48:10 +0300

    добавил тесты для pending состояний

--- src/store/project-data/project-data.test.ts
+++ src/store/project-data/project-data.test.ts
@@ -18,9 +18,21 @@ describe('Reducer:  projectData', () => {
       .toEqual({cameras: fakeCameras, pageCount: 1, promo: {}, isDataLoaded: false});
   });
 
+  it('should update isDataLoaded by load cameras', () => {
+    const state = {cameras: [], pageCount: 0, promo: {} as promoType, isDataLoaded: false};
+    expect(projectData.reducer(state, {type: fetchCamerasAction.pending.type, payload: fakeCameras}))
+      .toEqual({cameras: [], pageCount: 0, promo: {} as promoType, isDataLoaded: true});
+  });
+
   it('should update promo by load promo', () => {
     const state = {cameras: [], pageCount: 0, promo: {} as promoType, isDataLoaded: false};
     expect(projectData.reducer(state, {type: fetchPromoAction.fulfilled.type, payload: fakePromo}))
       .toEqual({cameras: [], pageCount: 0, promo: fakePromo, isDataLoaded: false});
+  });
+
+  it('should update isDataLoaded by load promo', () => {
+    const state = {cameras: [], pageCount: 0, promo: {} as promoType, isDataLoaded: false};
+    expect(projectData.reducer(state, {type: fetchPromoAction.pending.type, payload: fakePromo}))
+      .toEqual({cameras: [], pageCount: 0, promo: {} as promoType, isDataLoaded: true});
   });
 });
